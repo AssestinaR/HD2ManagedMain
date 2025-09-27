bafaacff05d04695a917442c55a9bb86fcc7c37e
AssestinaR <2438724037@qq.com>
2025-09-27 09:51:49 +0800

    remove wpfapp3

--- WpfApp3/Services/IRenameService.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.ObjectModel;
-
-namespace LiberTeaManager.Services
-{
-    public interface IRenameService
-    {
-        bool TryRename(object target, string newName, ObservableCollection<MainModItem> mods);
-    }
-}
